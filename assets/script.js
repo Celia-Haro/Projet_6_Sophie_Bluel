f59376830d33515c31c21e2079e21ca0c072117d
Celia Haro <charo@keldin.fr>
2024-08-28 18:54:32 +0200

    add function fetchCategories + generateCategories

--- assets/script.js
+++ assets/script.js
@@ -33,3 +33,46 @@ const generateProjects = (works) => {
 };
 
 fetchWorks().then((works) => generateProjects(works));
+
+// Fetch pour récupérer les catégories
+
+const fetchCategories = async () => {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    return await response.json();
+  } catch (error) {
+    console.log(
+      "Une erreur est survenue dans le processus de récupération des données des categories"
+    );
+    alert("Un problème de serveur est survenu");
+  }
+};
+
+// Générer les boutons dynamiquement
+
+const generateCategories = (categories) => {
+  const filters = document.querySelector("#portfolio .filters");
+
+  for (i = 0; i < categories.length; i++) {
+    const filter = document.createElement("button");
+    filter.classList.add("filter");
+    filter.innerText = categories[i].name;
+    filter.setAttribute("id", categories[i].id);
+    filters.appendChild(filter);
+    console.log(categories);
+
+    filter.addEventListener("click", () => {
+      // Enlever la classe active de tous les boutons
+      const allFilters = document.querySelectorAll(".filter");
+      allFilters.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      // Ajouter la classe active sur le bouton cliqué
+      filter.classList.add("active");
+      console.log("Bouton cliqué : ", filter.innerText);
+    });
+  }
+};
+
+fetchCategories().then((categories) => generateCategories(categories));
