cdadf9ba2861e8e560069625bd4ec08bc34e0612
Celia Haro <haro.celia@hotmail.fr>
2024-08-28 17:03:05 +0200

    changes functions JS + html delete figures

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,26 @@
 // Récupérer les données du back via l'API + appeler la fonction genererProjet
 
-async function fetchWorks() {
+const fetchWorks = async () => {
   try {
-    const reponse = await fetch("http://localhost:5678/api/works");
-    works = await reponse.json();
-    console.log(works);
+    const response = await fetch("http://localhost:5678/api/works");
+    return await response.json();
   } catch (error) {
     console.log(
       "Une erreur est survenue dans le processus de récupération des données des works"
     );
+    alert("Un problème de serveur est survenu");
   }
-  genererProjet(works);
-}
-
-fetchWorks();
+};
 
 // Création de la fonction pour générer les projets dynamiquement
 
-function genererProjet() {
+const generateProjects = (works) => {
   const gallery = document.querySelector("#portfolio .gallery");
   gallery.innerHTML = "";
   for (i = 0; i < works.length; i++) {
     const figure = document.createElement("figure");
-
     const image = document.createElement("img");
+
     image.setAttribute("src", works[i].imageUrl);
     figure.appendChild(image);
 
@@ -33,4 +30,6 @@ function genererProjet() {
 
     gallery.appendChild(figure);
   }
-}
+};
+
+fetchWorks().then((works) => generateProjects(works));
